146c69ccfca39e6774fcdca9e8d0070502e6c8c2
soutoigor <igor_souto@outlook.com>
2018-11-07 23:46:39 -0200

    cars listing

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 (function ($) {
-    'use strict';
+  'use strict';
 
-    var app = (function appController() {
-       return {
-         init: function init() {
-           this.companyInfo();
-           this.listCars();
-           this.initEvents();
+  var app = (function appController() {
+    return {
+      init: function init() {
+        this.companyInfo();
+        this.listCars();
+        this.initEvents();
 
-         },
-          createNewCar: function createNewCar() {
-          var $fragment = document.createDocumentFragment();
-          var $urlImage = $('[data-js="image"]').get().value;
+      },
+      createNewCar: function createNewCar() {
+        /*  var $fragment = document.createDocumentFragment();
           var $tr = document.createElement('tr');
           var $tdImage = document.createElement('td');
           var $image = document.createElement('img');
@@ -38,149 +37,182 @@
            $tr.appendChild($tdPlate);
            $tr.appendChild($tdColor);
            $tr.appendChild($tdRemove);
+            */
 
-           this.saveNewCar($urlImage, $tdBrand.textContent, $tdYear.textContent, $tdPlate.textContent, $tdColor.textContent);
+        var $urlImage = $('[data-js="image"]').get().value;
+        var $tdBrand = $('[data-js="brand-model"]').get().value;
+        var $tdYear = $('[data-js="year"]').get().value;
+        var $tdPlate = $('[data-js="plate"]').get().value;
+        var $tdColor = $('[data-js="color"]').get().value;
 
-          return $fragment.appendChild($tr);
-          },
-          initEvents: function initEvents(){
+        this.saveNewCar($urlImage, $tdBrand, $tdYear, $tdPlate, $tdColor);
+
+
+      },
+      initEvents: function initEvents() {
         //  this.formValidation();
-          $('[data-js="form-register"]').on( 'submit', this.handleSubmit );
+        $('[data-js="form-register"]').on('submit', this.handleSubmit);
 
+      },
+      handleSubmit: function handleSubmit(e) {
+        e.preventDefault();
 
-          },
-          handleSubmit: function handleSubmit(e){
-            e.preventDefault();
+        app.createNewCar();
+        app.listCars();
+        
+        //$tableCar.appendChild(app.createNewCar());
 
+      },
+      companyInfo: function companyInfo() {
 
-            var $tableCar = $('[data-js="table-car"]').get();
-            $tableCar.appendChild(app.createNewCar());
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', 'company.json', true);
+        ajax.send();
+
+        ajax.addEventListener('readystatechange', this.getCompanyInfo);
+
+      },
+
+      getCompanyInfo: function getCompanyInfo() {
+        if (app.isReady.call(this)) {
+          var data = JSON.parse(this.responseText);
+          var $companyName = $('[data-js="company-name"]');
+          var $companyPhone = $('[data-js="company-phone"]');
+          $companyName.get().textContent = data.name;
+          $companyPhone.get().textContent = data.phone;
+        }
+      },
+
+      isReady: function isReady() {
+        return this.readyState === 4 && this.status === 200;
+      },
+      removeCarRow: function removeCarRow() {
+        var $removeTd = $('[data-js="remove-car"]');
+
+        $removeTd.on('click', function () {
+          console.log(this.parentNode.parentNode.childNodes[3]);
+          console.log('Erase button was clicked!');
+          this.parentNode.remove(this.parentNode);
+          // ACHAR O CONTEUDO DA PLACA
+          app.deleteCar(this.parentNode.parentNode.childNodes[3].textContent);
+          
+        });
+      },
+      deleteCar: function deleteCar(plate) {
+        var ajax = new XMLHttpRequest();
+        ajax.open('DELETE', 'http://localhost:3000/car');
+        ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        ajax.send(`plate=${plate}`);
+
+        ajax.addEventListener('readystatechange', function () {
+          if (app.isReady.call(this)) {
+            app.listCars();
+          }
+        });
+      },
+      listCars: function listCars() {
+        app.clearTable();
+        var $tableCar = $('[data-js="table-car"]').get();
+
+        var get = new XMLHttpRequest();
+        get.open('GET', 'http://localhost:3000/car');
+        get.send();
+
+        get.addEventListener('readystatechange', function () {
+
+          if (app.isReady.call(this)) {
+            var cars = JSON.parse(get.responseText);
+            cars.forEach(function (car) {
+              console.log('-----------------')
+              console.log(car.image);
+              console.log(car.brandModel);
+              console.log(car.plate);
+              console.log(car.year);
+              console.log(car.color);
+
+              var $tr = document.createElement('tr');
+
+              $tr.innerHTML = `
+                <td><img src="${car.image}"</td>
+                <td>${car.brandModel}</td>
+                <td>${car.year}</td>
+                <td>${car.plate}</td>
+                <td>${car.color}</td>
+                <td><i class="fas fa-trash-alt" data-js="remove-car" style=\' cursor: pointer; \' ></i></td>
+               `;
+
+              $tableCar.appendChild($tr);
+            });
             app.removeCarRow();
 
-          },
-          companyInfo: function companyInfo() {
-
-            var ajax = new XMLHttpRequest();
-            ajax.open('GET', 'company.json', true);
-            ajax.send();
-
-            ajax.addEventListener('readystatechange', this.getCompanyInfo);
-
-          },
-
-          getCompanyInfo: function getCompanyInfo() {
-            if (app.isReady.call(this)){
-              var data = JSON.parse(this.responseText);
-              var $companyName = $('[data-js="company-name"]');
-              var $companyPhone = $('[data-js="company-phone"]');
-              $companyName.get().textContent = data.name;
-              $companyPhone.get().textContent = data.phone;
-            }
-
-        },
-
-        isReady: function isReady(){
-          return this.readyState === 4 && this.status === 200;
-        },
-        removeCarRow: function removeCarRow(){
-            var $removeTd = $('[data-js="remove-car"]');
-			
-            $removeTd.on('click', function(){
-              this.parentNode.remove(this.parentNode);
-              app.deleteCar(this.parentNode.childNodes[3].textContent);  
-            });
-        },
-        deleteCar: function deleteCar(plate){
-          var ajax = new XMLHttpRequest();
-          ajax.open('DELETE', 'http://localhost:3000/car');
-          ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-          ajax.send(`plate=${plate}`);
-
-          ajax.addEventListener('readystatechange', function(){
-            if (app.isReady.call(this)){              
-              app.listCars();
-            }
-          });
-        },
-        listCars: function listCars(){
-          var get = new XMLHttpRequest();
-          get.open('GET', 'http://localhost:3000/car');
-          get.send();
-
-          get.addEventListener('readystatechange', function(){
-
-            if (app.isReady.call(this)){
-              var cars = JSON.parse(get.responseText);
-              console.log('-----------------')
-              cars.forEach(function(car) {
-                console.log(car);
-              });
-             
-            }
-            
-           
-
-          });
-          
-        },
-        saveNewCar: function saveNewCar(image, brand, year, plate, color){
-          var save = new XMLHttpRequest();
-          save.open('POST', 'http://localhost:3000/car');
-          save.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-          save.send(`image=${image}&brandModel=${brand}&year=${year}&plate=${plate}&color=${color}`);
-
-          save.addEventListener('readystatechange', function(){
-            if (app.isReady.call(this)){
-              var feedbackMessage = JSON.parse(save.responseText);
-              app.showFeedbackMessage(feedbackMessage.message, feedbackMessage.success);
-              app.listCars();
-            }
-          });
-        },
-        showFeedbackMessage: function feedbackMessage(message, hasSuccess){
-          var $alertMessage = $('[data-js="feedback-message"]');
-          $alertMessage.get().style='display: block;'
-          if(hasSuccess){
-            $alertMessage.get().classList.add('alert-success');
-            $alertMessage.get().classList.remove('alert-danger');
           }
-          if(!hasSuccess){
-            $alertMessage.get().classList.add('alert-danger');
-            $alertMessage.get().classList.remove('alert-success');
+
+
+
+        });
+
+      },
+      saveNewCar: function saveNewCar(image, brand, year, plate, color) {
+        var save = new XMLHttpRequest();
+        save.open('POST', 'http://localhost:3000/car');
+        save.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        save.send(`image=${image}&brandModel=${brand}&year=${year}&plate=${plate}&color=${color}`);
+
+        save.addEventListener('readystatechange', function () {
+          if (app.isReady.call(this)) {
+            var feedbackMessage = JSON.parse(save.responseText);
+            app.showFeedbackMessage(feedbackMessage.message, feedbackMessage.success);
+          }
+        });
+      },
+      showFeedbackMessage: function feedbackMessage(message, hasSuccess) {
+        var $alertMessage = $('[data-js="feedback-message"]');
+        $alertMessage.get().style = 'display: block;'
+        if (hasSuccess) {
+          $alertMessage.get().classList.add('alert-success');
+          $alertMessage.get().classList.remove('alert-danger');
         }
-          $alertMessage.get().textContent = message;
-
-          setTimeout(function(){
-            $alertMessage.get().style='display: none;';
-          }, 2500);
+        if (!hasSuccess) {
+          $alertMessage.get().classList.add('alert-danger');
+          $alertMessage.get().classList.remove('alert-success');
         }
-        /*,
-        formValidation: function formValidation(){
+        $alertMessage.get().textContent = message;
 
-          //  if (form.checkValidity() === false) {
+        setTimeout(function () {
+          $alertMessage.get().style = 'display: none;';
+        }, 2500);
 
-          //       }
+      },
+      clearTable: function clearTable(){
+        $('[data-js="table-car"]').get().innerHTML = '';
+      }
+      /*,
+      formValidation: function formValidation(){
 
+        //  if (form.checkValidity() === false) {
 
-          $('[data-js="year"]').get().addEventListener('input', function(e){
-            $('[data-js="year"]').get().value = $('[data-js="year"]').get().value.replace(/\D/g, '');
-
-          });
-
-          $('[data-js="plate"]').get().addEventListener('blur', function(){
-            var regexPlate = /[a-zA-Z]{3}-[\d]{4}$/g;
-               if(!regexPlate.test($('[data-js="plate"]').get().value)){
-                $('[data-js="form-register"]').get().classList.add('was-validated');
-                return false;
-               }
-               return true;
-          });
-
-        }*/
+        //       }
 
 
+        $('[data-js="year"]').get().addEventListener('input', function(e){
+          $('[data-js="year"]').get().value = $('[data-js="year"]').get().value.replace(/\D/g, '');
 
-       
+        });
+
+        $('[data-js="plate"]').get().addEventListener('blur', function(){
+          var regexPlate = /[a-zA-Z]{3}-[\d]{4}$/g;
+             if(!regexPlate.test($('[data-js="plate"]').get().value)){
+              $('[data-js="form-register"]').get().classList.add('was-validated');
+              return false;
+             }
+             return true;
+        });
+
+      }*/
+
+
+
+
 
 
     };
